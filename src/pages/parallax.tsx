62482214064b95dea323c7083ee1b46c6650e846
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-05-04 20:28:11 +0800

    add usePrevious

--- src/pages/parallax.tsx
+++ src/pages/parallax.tsx
@@ -1,17 +1,9 @@
 import * as React from "react";
-import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 
 export interface Props {}
 
-const IndexPage: React.SFC<Props> = () => {
-  return (
-    <div>
-      <Parallax pages={2}>
-        <ParallaxLayer offset={1} style={{ backgroundColor: "red" }} />
-        <ParallaxLayer offset={2} style={{ backgroundColor: "blue" }} />
-      </Parallax>
-    </div>
-  );
+const ResumePage: React.SFC<Props> = () => {
+  return <div>Resume</div>;
 };
 
-export default IndexPage;
+export default ResumePage;
